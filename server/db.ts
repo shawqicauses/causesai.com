60b541c32fae6f7bfb7c3d81e6a99fc3c5e66f21
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-08-01 03:30:39 +0200

    New: Enhance through setting up `Prisma` as an ORM

---
+++ server/db.ts
@@ -0,0 +1,17 @@
+// DONE REVIEWING: GITHUB COMMIT
+
+import {PrismaClient} from "@prisma/client"
+
+const prismaClientSingleton = function prismaClientSingleton() {
+  return new PrismaClient()
+}
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>
+} & typeof global
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma
