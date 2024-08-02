de8583787cdbe32ca45b2868eb86751ee9ab771e
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-08-02 19:25:31 +0200

    New: Enhance through adding `SignUpFormProvider` file

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,7 +1,15 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
+
+import SignUpFormProvider from "../../../components/forms/sign-up/provider"
 
 const SignUpPage = function SignUpPage() {
-  return <div>Sign Up Page</div>
+  return (
+    <div className="w-full flex-1 py-36 md:px-16">
+      <div className="flex h-full flex-col gap-3">
+        <SignUpFormProvider />
+      </div>
+    </div>
+  )
 }
 
 export default SignUpPage
---
+++ components/forms/sign-up/provider.tsx
@@ -0,0 +1,9 @@
+// DONE REVIEWING: GITHUB COMMIT
+
+import {PropsWithChildren} from "react"
+
+const SignUpFormProvider = function SignUpFormProvider({children}: PropsWithChildren) {
+  return <div>Sign Up Form Provider {children}</div>
+}
+
+export default SignUpFormProvider
