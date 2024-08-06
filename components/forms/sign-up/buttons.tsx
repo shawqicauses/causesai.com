bdd715dd6c7f4df25de02e8f7d1b5c3c8c130677
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-08-06 20:21:01 +0200

    New: Enhance through adding rendering for step no. 2

--- components/forms/sign-up/buttons.tsx
+++ components/forms/sign-up/buttons.tsx
@@ -1,15 +1,17 @@
 "use client"
 
-// DONE REVIEWING: GITHUB COMMIT 2️⃣
+// DONE REVIEWING: GITHUB COMMIT 3️⃣
 
 import Link from "next/link"
 import {useFormContext} from "react-hook-form"
 import {useAuthentication} from "../../../contexts/authentication"
+import useSignUpForm from "../../../hooks/sign-up/use-sign-up.hook"
 import {Button} from "../../ui"
 
 const SignUpFormButtons = function SignUpFormButtons() {
-  const {stepCurrent, setStepCurrent} = useAuthentication()
   const {formState, getFieldState, getValues} = useFormContext()
+  const {stepCurrent, setStepCurrent} = useAuthentication()
+  const {onGenerateOTP} = useSignUpForm()
 
   const {isDirty: isName} = getFieldState("name", formState)
   const {isDirty: isEmail} = getFieldState("email", formState)
@@ -20,6 +22,30 @@ const SignUpFormButtons = function SignUpFormButtons() {
       <div className="flex w-full flex-col items-center gap-3">
         <Button type="submit" className="w-full">
           Create an account
+        </Button>
+        <p className="text-sm text-muted-foreground">
+          Do you have an account?{" "}
+          <Button variant="link" className="text-foreground !shadow-none" asChild>
+            <Link href="/auth/sign-in">Sign In</Link>
+          </Button>
+        </p>
+      </div>
+    )
+
+  if (stepCurrent === 2)
+    return (
+      <div className="flex w-full flex-col items-center gap-3">
+        <Button
+          type="button"
+          disabled={!isName || !isEmail || !isPassword}
+          className="w-full"
+          {...(isName &&
+            isEmail &&
+            isPassword && {
+              onClick: () =>
+                onGenerateOTP(getValues("email"), getValues("password"), setStepCurrent)
+            })}>
+          Continue
         </Button>
         <p className="text-sm text-muted-foreground">
           Do you have an account?{" "}
