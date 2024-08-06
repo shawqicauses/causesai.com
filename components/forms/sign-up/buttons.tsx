684a9cbae41adaef95a29a4bbca9cdb5a89af31d
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-08-06 19:12:42 +0200

    New: Enhance through adding rendering for step no. 1

--- components/forms/sign-up/buttons.tsx
+++ components/forms/sign-up/buttons.tsx
@@ -1,6 +1,35 @@
-// DONE REVIEWING: GITHUB COMMIT
+"use client"
+
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
+
+import Link from "next/link"
+import {useFormContext} from "react-hook-form"
+import {useAuthentication} from "../../../contexts/authentication"
+import {Button} from "../../ui"
 
 const SignUpFormButtons = function SignUpFormButtons() {
+  const {stepCurrent, setStepCurrent} = useAuthentication()
+  const {formState, getFieldState, getValues} = useFormContext()
+
+  const {isDirty: isName} = getFieldState("name", formState)
+  const {isDirty: isEmail} = getFieldState("email", formState)
+  const {isDirty: isPassword} = getFieldState("password", formState)
+
+  if (stepCurrent === 3)
+    return (
+      <div className="flex w-full flex-col items-center gap-3">
+        <Button type="submit" className="w-full">
+          Create an account
+        </Button>
+        <p className="text-sm text-muted-foreground">
+          Do you have an account?{" "}
+          <Button variant="link" className="text-foreground !shadow-none" asChild>
+            <Link href="/auth/sign-in">Sign In</Link>
+          </Button>
+        </p>
+      </div>
+    )
+
   return <div>Sign Up Form Buttons</div>
 }
 
