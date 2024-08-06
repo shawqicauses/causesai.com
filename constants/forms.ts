384e9f11870ef83c6d62c90d03d802a4fd5c334b
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-08-07 00:09:03 +0200

    New: Enhance through adding `SIGN_UP_INFORMATION_FORM`

---
+++ constants/forms.ts
@@ -0,0 +1,41 @@
+// DONE REVIEWING: GITHUB COMMIT
+
+export const SIGN_UP_INFORMATION_FORM = [
+  {
+    id: 1,
+    controllerType: "input",
+    inputType: "text",
+    name: "fullName",
+    placeholder: "Full name"
+  },
+  {
+    id: 2,
+    controllerType: "input",
+    inputType: "email",
+    name: "email",
+    placeholder: "Email"
+  },
+  {
+    id: 3,
+    controllerType: "input",
+    inputType: "email",
+    name: "emailConfirmation",
+    placeholder: "Confirm email"
+  },
+  {
+    id: 4,
+    controllerType: "input",
+    inputType: "password",
+    name: "password",
+    placeholder: "Password"
+  },
+  {
+    id: 5,
+    controllerType: "input",
+    inputType: "password",
+    name: "passwordConfirmation",
+    placeholder: "Confirm password"
+  }
+]
+
+export default {SIGN_UP_INFORMATION_FORM}
