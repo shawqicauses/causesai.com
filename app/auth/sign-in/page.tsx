fb504d01a618a01f7bcf6c7463e52604caa2feb2
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-08-08 01:04:24 +0200

    New: Enhance through adding content to sign-in page

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,7 +1,29 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
+
+import Link from "next/link"
+import SignInFormProvider from "../../../components/forms/sign-in/provider"
+import {Button} from "../../../components/ui"
 
 const SignInPage = function SignInPage() {
-  return <div>Sign In Page</div>
+  return (
+    <div className="w-full flex-1">
+      <div className="flex h-full flex-col gap-3">
+        <SignInFormProvider>
+          <div className="flex w-full flex-col items-center gap-3">
+            <Button type="submit" className="w-full">
+              Sign in
+            </Button>
+            <p className="text-sm text-muted-foreground">
+              Do not have an account?{" "}
+              <Button variant="link" className="text-foreground !shadow-none" asChild>
+                <Link href="/auth/sign-up">Sign Up</Link>
+              </Button>
+            </p>
+          </div>
+        </SignInFormProvider>
+      </div>
+    </div>
+  )
 }
 
 export default SignInPage
